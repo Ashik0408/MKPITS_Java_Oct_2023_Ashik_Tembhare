bd47979f542d56f0c40a7854f924671d64d0e8c3
Ashik0408 <ashikt128@gmail.com>
2023-11-26 20:09:49 +0530

    Hotel Managnment Progrsm Added

---
+++ hotelManagnment.cpp
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	char bookRoom,orderContinue;
+	int i,count=1,orderNo,qty,rate,teaTotal;
+	
+	printf("Welcome to >>>>>>>\n");
+	printf("Do you want to Book Room :- y/n \n");
+	scanf("%c",&bookRoom);
+	if(bookRoom=='y')
+	{
+		printf("Please Collect Your keys from Reception \n");
+		printf("Welcome To Shri Hotel....Your Room has been Booked \n");
+		
+		for(i=1;count>=i;i++)
+		{
+			printf("Food Menu \n 1.Tea \n 2.Coffee \n 3.Samosa \n 4.Maharastrian Thali \n");
+			scanf("%d",&orderNo);
+			
+			switch(orderNo)
+			{
+				case 1:
+					printf("\nYou have selected Tea.\n Enter the quantity :");
+   					scanf("%d",&qty);
+    				rate=5;
+    				teaTotal=qty*rate;
+			}
+			printf("Do you want to continue your order :y/n \n");
+			scanf("%c",&orderContinue);
+			if(orderContinue=='y')
+			{
+				count=count+1;
+			}
+			
+			
+		}
+	}
+	
+		
+	
+	printf("Total Food Amt :-%d",teaTotal);
+}
---
+++ hotelMang.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+
+int menu();
+
+int main()
+{
+	int total=0;
+	printf("Welcome to >>>>>>>\n");
+//	printf("Do you want to Book Room :- y/n \n");
+	
+	total =menu();
+	printf("Total :-%d",&total);
+}
+
+int menu()
+{
+	int orderNo,qty,rate,teaTotal=0;
+	printf("Food Menu \n 1.Tea \n 2.Coffee \n 3.Samosa \n 4.Maharastrian Thali \n");
+			scanf("%d",&orderNo);
+			
+			switch(orderNo)
+			{
+				case 1:
+					printf("\nYou have selected Tea.\n Enter the quantity :");
+   					scanf("%d",&qty);
+    				rate=5;
+    				teaTotal=qty*rate;
+    				break;
+			}
+	return teaTotal;
+}
